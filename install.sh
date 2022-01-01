e013e8d9a83859ba60ebed698885c96977a0b726
kjeller <kallestralman@gmail.com>
2022-01-01 19:53:03 +0100

    Remove deprecated configs and modify install script

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-# Create symlinks for all dotfiles
-ln -frs vim/.vimrc ~/.vimrc
-ln -frs git/.gitconfig ~/.gitconfig
-ln -frs tmux/.tmux.conf ~/.tmux.conf
-ln -frs i3/i3config ~/.config/i3/config
+#!/bin/bash
+
+PWD=`pwd`
+
+# Create symlinks for all config files
+cp -nrs $PWD/vim/.vimrc ~/.vimrc
+cp -nrs $PWD/git/.gitconfig ~/.gitconfig
+cp -nrs $PWD/tmux/.tmux.conf ~/.tmux.conf
+cp -nrs $PWD/tmuxinator ~/.config/tmuxinator
+

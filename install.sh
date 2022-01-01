07b2f911cb60b561eaa0029d12f19868dab3ea24
kjeller <kallestralman@gmail.com>
2022-01-01 19:55:29 +0100

    Add tmuxinator configs

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 PWD=`pwd`
 
 # Create symlinks for all config files
-cp -nrs $PWD/vim/.vimrc ~/.vimrc
-cp -nrs $PWD/git/.gitconfig ~/.gitconfig
-cp -nrs $PWD/tmux/.tmux.conf ~/.tmux.conf
-cp -nrs $PWD/tmuxinator ~/.config/tmuxinator
+cp -frs $PWD/vim/.vimrc ~/.vimrc
+cp -frs $PWD/git/.gitconfig ~/.gitconfig
+cp -frs $PWD/tmux/.tmux.conf ~/.tmux.conf
+cp -frs $PWD/tmuxinator/ ~/.config/
 

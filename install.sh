fcb3c73873f00bf14e588c0f43aa75f195afa6d2
kjeller <kallestralman@gmail.com>
2022-10-06 10:53:41 +0200

    Add git prompt script
    Source this in ~/.bashrc file when a useable config is found

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ PWD=`pwd`
 # Create symlinks for all config files
 cp -frs $PWD/vim/.vimrc ~/.vimrc
 cp -frs $PWD/git/.gitconfig ~/.gitconfig
+cp -frs $PWD/git/.git-prompt.sh ~/.git-prompt.sh
 cp -frs $PWD/tmux/.tmux.conf ~/.tmux.conf
 cp -frs $PWD/tmuxinator/ ~/.config/
 

bd14d8a8474c6a86fa09abe1d40fb8166bcbd1f9
kjeller <kallestralman@gmail.com>
2022-10-15 23:01:22 +0200

    Add plugins to tmux config

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ PWD=`pwd`
 cp -frs $PWD/vim/.vimrc ~/.vimrc
 cp -frs $PWD/git/.gitconfig ~/.gitconfig
 cp -frs $PWD/git/.git-prompt.sh ~/.git-prompt.sh
+cp -frs $PWD/tmux/plugins ~/.tmux/
 cp -frs $PWD/tmux/.tmux.conf ~/.tmux.conf
 cp -frs $PWD/tmuxinator/ ~/.config/
 

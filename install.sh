01261bf0c96f452ccd86980c7f5324023d25d2f8
kjeller <kallestralman@gmail.com>
2023-07-03 12:16:52 +0200

    Add foot term config and way to determine what term config to install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PWD=`pwd`
+CONFIG=~/.config
 
 # Create symlinks for all config files
 cp -frs $PWD/vim/.vimrc ~/.vimrc
@@ -8,5 +9,17 @@ cp -frs $PWD/git/.gitconfig ~/.gitconfig
 cp -frs $PWD/git/.git-prompt.sh ~/.git-prompt.sh
 cp -frs $PWD/tmux/plugins ~/.tmux/
 cp -frs $PWD/tmux/.tmux.conf ~/.tmux.conf
-cp -frs $PWD/tmuxinator/ ~/.config/
+cp -frs $PWD/tmuxinator/ $CONFIG
 
+case $TERM in
+  "foot-extra")
+    echo -n "Using foot terminal config"
+    cp -frs $PWD/foot/ $CONFIG/foot/
+    ;;
+  "linux" | "screen"*)
+    echo -n "Warning: Running in virtual terminal. Cannot determine what config to use.. skipping"
+    ;;
+  *)
+    echo -n "No specific terminal config to handle.. skipping"
+    ;;
+esac

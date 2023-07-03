71e3e03f83b8f64d7afb3730ffdb88e4bdeaea1a
kjeller <kallestralman@gmail.com>
2023-07-03 13:53:41 +0200

    Add rclone config and service to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 PWD=`pwd`
 CONFIG=~/.config
+SYSTEMD_USER_SERVICE_PATH=/etc/systemd/user
 
 # Create symlinks for all config files
 cp -frs $PWD/vim/.vimrc ~/.vimrc
@@ -11,15 +12,66 @@ cp -frs $PWD/tmux/plugins ~/.tmux/
 cp -frs $PWD/tmux/.tmux.conf ~/.tmux.conf
 cp -frs $PWD/tmuxinator/ $CONFIG
 
+# Determine what terminal configs to install based on terminal capabilities
+# Not really ideal since different terminals may use same capability.
 case $TERM in
   "foot-extra")
-    echo -n "Using foot terminal config"
+    echo "Using foot terminal config"
     cp -frs $PWD/foot/ $CONFIG/foot/
     ;;
   "linux" | "screen"*)
-    echo -n "Warning: Running in virtual terminal. Cannot determine what config to use.. skipping"
+    echo "Warning: Running in virtual terminal. Cannot determine what config to use.. skipping"
     ;;
   *)
-    echo -n "No specific terminal config to handle.. skipping"
+    echo "No specific terminal config to handle.. skipping"
     ;;
 esac
+
+# Rclone private config and service setup
+# see https://www.stralman.dev/post/mount_with_rclone/ if you want to configure this yourself
+if [ -d "$PWD/rclone" ]; then
+  RCLONE_SERVICE=rclone@.service
+
+  echo "rclone config mount paths may require a path that needs to be created by sudo user.."
+  if [[ "$EUID" = 0 ]]; then
+    echo "Already sudo user.. continuing"
+  else
+    sudo -k
+    if sudo false; then
+      echo "Error: sudo is required for this step"
+      exit 1
+    fi
+  fi
+
+  if [ ! -f "$SYSTEMD_USER_SERVICE_PATH/$RCLONE_SERVICE" ]; then
+    sudo cp -frs $PWD/rclone/service/$RCLONE_SERVICE $SYSTEMD_USER_SERVICE_PATH/$RCLONE_SERVICE
+
+    echo -n "Telling systemd dameon to look for new services"
+    systemctl --user daemon-reload
+    echo "..done"
+  else
+    echo "rclone user service config $RCLONE_SERVICE already exists.. skipping"
+  fi
+  
+  mkdir -p ~/.config/rclone/
+  cp -frs $PWD/rclone/conf/* ~/.config/rclone/
+  
+
+  for i in $PWD/rclone/conf/*.env; do
+    if [ -f "$i" ]; then
+      echo "Sourcing file to determine what mount path to create $i"
+      source "$i"
+      echo "Creating mount path $MOUNT_DIR"
+      sudo mkdir -p "$MOUNT_DIR"
+      sudo chown $USER $MOUNT_DIR
+      SERVICE_NAME="`basename $MOUNT_DIR`"
+      echo "Start templated service for $SERVICE_NAME remote"
+      systemctl --user enable rclone@$SERVICE_NAME
+      systemctl --user start rclone@$SERVICE_NAME
+    fi
+  done
+
+else
+  echo "Could not find $PWD/rclone.. skipping"
+fi
+
